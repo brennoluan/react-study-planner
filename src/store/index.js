34b4a4dad71eb952d2f3af8aae41de7b092a0ead
Brenno Luan <brennoluan8530@gmail.com>
2026-05-17 23:06:57 -0300

    Persist Redux state to `localStorage` and add throttled save logic

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import {configureStore} from '@reduxjs/toolkit'
 import taskReducer from "./slices/taskSlice.js";
 import themeReducer from "./slices/themeSlice.js";
 import analyticReducer from "./slices/analyticSlice.js";
+import {loadState, saveState, throttle} from "./localStorage.js";
+
+const preloadedState = loadState()
 
 export const store = configureStore({
     reducer: {
@@ -9,5 +12,18 @@ export const store = configureStore({
         theme: themeReducer,
         analytics: analyticReducer
     },
-    devTools: process.env.NODE_ENV !== 'production'
+    devTools: process.env.NODE_ENV !== 'production',
+    preloadedState,
+})
+
+const throttledSaveState = throttle((state) => {
+    saveState({
+        tasks: state.tasks,
+        theme: state.theme,
+        analytics: state.analytics,
+    })
+}, 1000)
+
+store.subscribe(() => {
+    throttledSaveState(store.getState())
 })
