2dd22c15aa0adece65b45c3fffb9cd803777dd8b
Brenno Luan <brennoluan8530@gmail.com>
2026-05-17 22:25:38 -0300

    Integrate `analyticSlice` into Redux store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import taskReducer from "./slices/taskSlice.js";
 import themeReducer from "./slices/themeSlice.js";
+import analyticReducer from "./slices/analyticSlice.js";
 
 export const store = configureStore({
     reducer: {
         tasks: taskReducer,
-        theme: themeReducer
+        theme: themeReducer,
+        analytics: analyticReducer
     },
 })
